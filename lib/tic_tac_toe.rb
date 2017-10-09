d4bd44e97a7a19d5e1e1583b01ac26f7664fd40c
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-10-09 17:27:44 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -58,4 +58,13 @@ class TicTacToe
       end
     end
   end
+
+  def won?
+  WIN_COMBINATIONS.detect do |combo|
+    @board[combo[0]] == @board[combo[1]] &&
+    @board[combo[1]] == @board[combo[2]] &&
+    position_taken?(combo[0])
+  end
+end
+
 end
