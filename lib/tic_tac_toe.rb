6bdb6e70feedae482ea6f26bc2b8158a3f823c02
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-10-09 17:28:30 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -60,11 +60,14 @@ class TicTacToe
   end
 
   def won?
-  WIN_COMBINATIONS.detect do |combo|
-    @board[combo[0]] == @board[combo[1]] &&
-    @board[combo[1]] == @board[combo[2]] &&
-    position_taken?(combo[0])
+    WIN_COMBINATIONS.detect do |combo|
+      @board[combo[0]] == @board[combo[1]] &&
+      @board[combo[1]] == @board[combo[2]] &&
+      position_taken?(combo[0])
+    end
   end
-end
+  def full?
+    @board.all? { |token| token == "X" || token == "O" }
+  end
 
 end
